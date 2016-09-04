c14ed5457d6b1248931c37e6465f694e13d08bd5
jc533 <yub612@gmail.com>
2016-09-04 11:41:36 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 var bgImg = document.createElement("img");
 bgImg.src = "images/map.png";
+var ememy = document.createElement("img");
+ememy.src = "images/rukia.gif";
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 function draw(){
   ctx.drawImage(bgImg,0,0);
 }
+function draw2(){
+  ctx.drawImage(ememy,0,0);
+}
 setTimeout(draw,1000);
+setTimeout(draw2,1000);
