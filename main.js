8767944dcf59edaf574a8088cc3368c512ad79bc
jc533 <yub612@gmail.com>
2016-09-04 11:44:26 +0800

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function draw(){
   ctx.drawImage(bgImg,0,0);
 }
 function draw2(){
-  ctx.drawImage(ememy,50,50);
+  ctx.drawImage(ememy,Math.random()*10,Math.random()*10);
 }
 setTimeout(draw,1000);
 setTimeout(draw2,1000);
