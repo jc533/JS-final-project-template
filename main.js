705dabac95f023d7f0b33832e9939d11efc1bc2b
jc533 <yub612@gmail.com>
2016-09-04 11:48:00 +0800

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,6 @@ var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 function draw(){
   ctx.drawImage(bgImg,0,0);
-  ctx.drawImage(ememy,Math.random()*500,Math.random()*500);
+  ctx.drawImage(ememy,Math.random()*500,Math.random()*100);
 }
 setTimeout(draw,1000);
