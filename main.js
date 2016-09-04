10330c0e9a0374748e3e06bdee94469ff9478af7
jc533 <yub612@gmail.com>
2016-09-04 11:31:57 +0800

    Update main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+var bgImg = document.createElement("img);
+bgImg.src = "images/map.png";
+var canvas = document.getElementById("game-canvas");
+var ctx = canvas.getContext("2d");
+function draw(){
+  ctx.drawImage(bgImg,0,0);
+}
+draw();
