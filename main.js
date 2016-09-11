d8799335ed29889bda86a3789833843b65334530
jc533 <yub612@gmail.com>
2016-09-11 12:34:17 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var bgImg = document.createElement("img");
-bgImg.src = "images/gamemap.png";
+bgImg.src = "images/map.png";
 var eImg = document.createElement("img");
 eImg.src = "images/rukia.gif";
 var towbtnImg = document.createElement("img");
