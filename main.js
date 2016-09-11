5cca304bfafbb1a708c70a3cdb1602029bb34b41
jc533 <yub612@gmail.com>
2016-09-11 11:31:00 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,20 +4,31 @@ var eImg = document.createElement("img");
 eImg.src = "images/rukia.gif";
 var tImg = document.createElement("img");
 tImg.src = "images/tower-btn.png";
+var towImg = document.createElement("img");
+eImg.src = "images/tower.png";
+
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
+var tower{
+  x:0,
+  y:0
+};
 var ememy = {
   x:10,
   y:10,
   v:[1,1]
 };
-
 function draw(){
   ctx.drawImage(bgImg,0,0);
   ctx.drawImage(eImg,ememy.x,ememy.y);
   ctx.drawImage(tImg,640-64,480-64,64,64);
+  ctx.drawImage(towImg,tower.x,tower.y);
 }
 setInterval(draw,16);
+$("game-canvas").on("mousemove",function(event){
+  tower.x = event.offsetX
+  tower.y = event.offsetY
+});
 $("body").on("keypress",key);
 function key(event){
   console.log(event.which)
