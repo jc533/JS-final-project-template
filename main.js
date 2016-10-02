82d956683420fc4b8cd4b710e4571640ebee6b83
jc533 <yub612@gmail.com>
2016-10-02 12:17:30 +0800

    Update main.js

--- main.js
+++ main.js
@@ -26,6 +26,7 @@ var enemy = {
     if(isCollided(enemyPath[this.pathDes].x,enemyPath[this.pathDes].y,this.x,this.y,this.speedx/FPS,this.speedy/FPS) == true){
       this.x = enemyPath[this.pathDes].x;
       this.y = enemyPath[this.pathDes].y;
+      console.log(this.x,this.y)
       this.pathDes ++;
       if(this.x == enemyPath[this.pathDes].x){
         this.speedx = 0;
