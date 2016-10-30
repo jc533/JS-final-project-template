baff2fecc840f8d5017ac5c8d2c32c9108ead827
jc533 <yub612@gmail.com>
2016-10-30 11:24:33 +0800

    Update main.js

--- main.js
+++ main.js
@@ -101,7 +101,7 @@ function attack(x1,y1,x2,y2){
   ctx.beginPath();
   ctx.moveTo(x1,y1);
   ctx.lineTo(x2,y2);
-  ctx.strokeStyle = #000000;
+  ctx.strokeStyle = "red";
   ctx.lineWidth = 30;
   ctx.stroke();
 }
